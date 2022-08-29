5a723cdd6ff03b1a0fb892617cfc1a45d3435927
Sushmita <sushmitakumari363@gmail.com>
2022-08-29 23:40:22 +0530

    feat: server connected

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ var txtinput=document.querySelector('#txt-input');
 var btnclicked=document.querySelector('#btnClicked');
 var outputDiv=document.querySelector('#output-div');
 
+var serverurl="https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text){
+    return serverurl +"?" +"text=" + text;
+}
+
+function errorHandler(error){
+console.log("error happened");
+alert("Something is wrong with server");
+}
+
 function clickEventHandler(){
-    outputDiv.innerHTML="translate "+ txtinput.value;
+    var inputText=txtinput.value;
+
+    fetch(getTranslationUrl(inputText))
+    .then(response=>response.json)
+    .then(json=>{
+        var translatedtext=json.content.translated;
+        outputDiv.innerHTML=translatedtext;
+    })
+    .catch(errorHandler)
+    
 }
 
 btnclicked.addEventListener('click',clickEventHandler);
